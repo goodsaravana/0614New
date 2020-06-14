7b2cb88d5200ad14ff88e617180eda65bf92e8c4
goodsaravana <goodsaravana@gmail.com>
2020-06-14 11:11:09 +0530

    Modify GitHub SYSO

--- Project14/src/packone/FirstClass.java
+++ Project14/src/packone/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Simple Program");
 		System.out.println("Hi this is GIT Check");
+		System.out.println("GIT Hub Practice");
 
 	}
 
